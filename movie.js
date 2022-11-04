9d66fbb64ee855c44bef962512054dc07c402880
John-Michael Krsak <jmkrsak@gmail.com>
2022-11-03 23:24:34 -0400

    added carousel pulling from giphy api to render gif based on title in movie array

--- movie.js
+++ movie.js
@@ -192,7 +192,7 @@
 
 
 
-//Get Method
+//Opening Loader Screen
 $('#shadow').css('display', 'none')
 let loading = document.createElement('div')
 loading.id = 'loading'
@@ -201,22 +201,27 @@ document.body.append(loading)
 setTimeout(function () {
     loading.remove()
     $('#shadow').css('display', 'block')
-}, 4000)
-
+}, 2000)
+//Get Method
+let array = []
 function getHandler() {
+    array = []
     fetch('https://little-thundering-jump.glitch.me/movies')
         .then(res => res.json())
         .then(data => {
+            console.log(data);
             $('.row').empty();
             $('#movies').empty()
             data.forEach(x => {
+
+                array.push(x)
                 let html = "";
-                html += `<div id=${x.id} class="col-3"><p>Title: ${x.title}</p><p>Rating: ${x.rating}</p>`;
+                html += `<div id=${x.id} class="col-3 mb-3 bg-dark text-light"><p>Title: ${x.title}</p><p>Rating: ${x.rating}</p><p>Genre: ${x.genre}</p>`;
                 html += `<button id=${x.id}>x</button></div>`;
 
                 $('.row').append(html);
                 let html2 = "";
-                html2 += `<option id=${x.id}>${x.title} - ${x.rating}</option>`;
+                html2 += `<option id=${x.id}>${x.title} - ${x.rating} - ${x.genre}</option>`;
                 $('#movies').append(html2)
 
 
@@ -233,14 +238,62 @@ function getHandler() {
 getHandler()
 
 
+let gifURL;
+let count = 0;
+function getCarousel() {
+    fetch('https://little-thundering-jump.glitch.me/movies')
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(pull => {
+                let title = pull.title
+                let id = pull.id
+                fetch(`https://api.giphy.com/v1/gifs/search?api_key=${GIF_KEY}&q=${title}&limit=25&offset=0&rating=g&lang=en`, {
+                    mode: "cors",
+                    dataType: "jsonp",
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    }
+                })
+                    .then(response => response.json())
+                    .then(json => {
+                        gifURL = json.data[0].images.original.url;
+                        console.log(gifURL)
+                        generateHTML(gifURL)
+                    })
+            })
+        })
+        .catch(error => console.error(error));
+    setTimeout(function () {
+    }, 2000)
+}
+getCarousel()
+function generateHTML(gifURL){
+    if (count === 0) {
+        let html = ``;
+        html += `<div class="carousel-item active">
+       <img src=${gifURL} id="caro" class="d-block w-100" alt="...">
+   </div>`;
+        $("#newOutPut").append(html);
+        count++
+    } else {
+        let html = ``;
+        html += `<div class="carousel-item">
+       <img src=${gifURL} class="d-block w-100" alt="...">
+   </div>`;
+        $("#newOutPut").append(html);
+        count++
+    }
+}
+
 // Post Method
 
-function postHandler(t, r) {
+function postHandler(t, r, g) {
     fetch('https://little-thundering-jump.glitch.me/movies', {
         method: 'POST',
         body: JSON.stringify({
             title: t,
-            rating: r
+            rating: r,
+            genre: g
         }),
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
@@ -253,8 +306,9 @@ function postHandler(t, r) {
 $('#submit').click(function (e) {
     console.log(this);
     let t = document.getElementById('inputTitle').value;
-    let r = document.getElementById('inputrRating').value;
-    postHandler(t, r)
+    let r = document.getElementById('inputeRating').value;
+    let g = document.getElementById('inputGenre').value;
+    postHandler(t, r, g)
 })
 
 
@@ -268,13 +322,13 @@ function deleteHandler(id) {
 
     })
 }
-
+//Adding event listeners to dle
 function setButtons() {
 
     let xBtn = document.querySelectorAll('button')
     xBtn.forEach(x => {
         x.addEventListener('click', function (e) {
-            console.log(this.id);
+
             deleteHandler(this.id)
         })
     })
@@ -282,38 +336,44 @@ function setButtons() {
 // adding event listeners to generate forms/button pulling data from events to populate PATCH METHOD
 let dropDwnBox = document.querySelector('#movies')
 dropDwnBox.addEventListener('change', function (e){
-    let findTitle = document.querySelector("select").value.split("-")[0]
-    let findRating = document.querySelector("select").value.split("-")[1]
+    let findTitle = document.querySelector("select").value.split("-")[0];
+    let findRating = document.querySelector("select").value.split("-")[1];
+    let findGenre = document.querySelector('select').value.split('-')[2];
     let getId = $(this).children(":selected").attr("id")
     $("#new-forms").empty()
     html = ""
     html += `<label for="newTitle">
            Title: <input type="text" name="newTitle" id="newTitle" value="${findTitle}">
        </label>
+       <label for="newGenre">
+        Genre: <input type="text" name="genre" id="newGenre" value="${findGenre}">
+       </label>
        <label for="newRating">
            Rating: <input type="text" name="newRating" id="newRating" value=${findRating}>
        </label>
        <label for="newSubmit">
-           <input type="button" name="submit" id="newSubmit" value="edit">
+           <input type="button" name="submit" id="newSubmit" value="Edit">
        </label>`
     $("#new-forms").append(html);
     let editBtn = document.querySelector('#newSubmit')
     editBtn.addEventListener('click', function(e){
         let xTitle = document.querySelector('#newTitle').value
         let xRating = document.querySelector('#newRating').value
+        let xGenre = document.querySelector('#newGenre').value
 
-        editPatch(xTitle, xRating, getId)
+        editPatch(xTitle, xRating, getId, xGenre)
     })
 })
 
 // PATCH METHOD
-function editPatch(title, rating, id) {
-    $('#new-forms').remove()
+function editPatch(title, rating, id, genre) {
+    $('#new-forms').empty()
     fetch(`https://little-thundering-jump.glitch.me/movies/${id}`, {
         method: 'PATCH',
         body: JSON.stringify({
             title: title,
-            rating: rating
+            rating: rating,
+            genre: genre
 
         }),
         headers: {
@@ -327,29 +387,120 @@ function editPatch(title, rating, id) {
 }
 
 
+//Search Movies
+function search_Movie() {
+    let input = document.getElementById('search').value;
+    input = input.toLowerCase();
+    let filteredMovies = [];
+    array.forEach(function(movie) {
+        if (movie.title.toLowerCase().includes(input)) {
+            filteredMovies.push(movie);
+        }
+    });
+    $('.row').innerHTML = renderMovies(filteredMovies);
+    setButtons()
+}
+
+let searchBar = document.querySelector('#search');
+searchBar.addEventListener('keyup', search_Movie);
+
+function renderMovie(movie) {
+
+    let html = "";
+    html += `<div id=${movie.id} class="col-3 mb-3 bg-dark text-light"><p>Title: ${movie.title}</p><p>Rating: ${movie.rating}</p><p>Genre: ${movie.genre}</p>`;
+    html += `<button id=${movie.id}>x</button></div>`;
+    return html
+}
+
+function renderMovies(movies) {
+    $('.row').empty()
+    let html = '';
+    for (let i = 0; i < movies.length; i++) {
+        html += renderMovie(movies[i]);
+    }
+    $('.row').append(html)
+}
+
+
+let range = document.getElementById('ratings')
+range.addEventListener('change', function (e) {
+
+    search_Range(this.value)
+
+})
+
+function search_Range(range) {
+
+    if (range === 'all') {
+        getHandler()
+    }
+    else {
+        let filteredMovies = [];
+        array.forEach(function(movie) {
+            if (movie.rating === range) {
+                filteredMovies.push(movie);
+            }
+        });
+        $('.row').innerHTML = renderMovies(filteredMovies);
+        setButtons()
+    }
+
+
+}
+
+
+function search_Genre() {
+    let input = document.getElementById('searchGenre').value;
+    input = input.toLowerCase();
+    let filteredMovies = [];
+    array.forEach(function(movie) {
+        if (movie.genre.toLowerCase().includes(input)) {
+            filteredMovies.push(movie);
+        }
+    });
+    $('.row').innerHTML = renderMovies(filteredMovies);
+    setButtons()
+}
+
+let genreSearch = document.querySelector('#searchGenre');
+genreSearch.addEventListener('keyup', search_Genre);
+
+
+
+
+// setTimeout(function () {
+//     fetch('http://www.omdbapi.com/?t=alien&apikey=df2cac18&')
+//         .then((response) => response.json())
+//         .then((json) => {
+//             console.log(json)
+//             // let test = document.getElementById('3')
+//
+//             // test.style.backgroundImage = `url('${json.Poster}')`;
+//             // $(test).css({'background-size': 'cover',
+//             //
+//             //     'background-repeat': 'no-repeat',
+//             //
+//             // 'background-position': 'center'
+//             // })
 //
 //
-//
-// //Patch Method
-//
-//
-// // fetch('https://little-thundering-jump.glitch.me/movies/4', {
-// //     method: 'PATCH',
-// //     body: JSON.stringify({
-// //         title: 'foo',
-// //         rating: 5
-// //
-// //     }),
-// //     headers: {
-// //         'Content-type': 'application/json; charset=UTF-8',
-// //     },
-// // })
-// //     .then((response) => response.json())
-// //     .then((json) => console.log(json));
-//
-//
-//
-//
+//         });
+// }, 5000)
+
+let hide = document.getElementById('hide');
+hide.addEventListener('click', function (e) {
+    e.preventDefault();
+    let unhide = document.querySelectorAll('#unhide');
+    unhide.forEach(x => {
+        $(x).toggleClass('none')
+        $('#ratings').toggleClass('none')
+    })
+
+})
+
+
+
+
 // //PUT Method
 //
 //
