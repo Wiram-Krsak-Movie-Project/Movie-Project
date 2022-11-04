c787ccf11b2aaa6e63c1f991994fcd7c4d5c96e4
Matthew Wiram <matthew.wiram551@gmail.com>
2022-11-03 22:36:03 -0400

    almost done

--- movie.js
+++ movie.js
@@ -201,17 +201,20 @@ document.body.append(loading)
 setTimeout(function () {
     loading.remove()
     $('#shadow').css('display', 'block')
-}, 4000)
+}, 2000)
 //Get Method
-let array;
+let array = []
 function getHandler() {
+    array = []
     fetch('https://little-thundering-jump.glitch.me/movies')
         .then(res => res.json())
         .then(data => {
-
+            console.log(data);
             $('.row').empty();
             $('#movies').empty()
             data.forEach(x => {
+
+                array.push(x)
                 let html = "";
                 html += `<div id=${x.id} class="col-3 mb-3 bg-dark text-light"><p>Title: ${x.title}</p><p>Rating: ${x.rating}</p><p>Genre: ${x.genre}</p>`;
                 html += `<button id=${x.id}>x</button></div>`;
@@ -256,7 +259,7 @@ function postHandler(t, r, g) {
 $('#submit').click(function (e) {
     console.log(this);
     let t = document.getElementById('inputTitle').value;
-    let r = document.getElementById('inputrRating').value;
+    let r = document.getElementById('inputeRating').value;
     let g = document.getElementById('inputGenre').value;
     postHandler(t, r, g)
 })
@@ -278,7 +281,7 @@ function setButtons() {
     let xBtn = document.querySelectorAll('button')
     xBtn.forEach(x => {
         x.addEventListener('click', function (e) {
-            console.log(this.id);
+
             deleteHandler(this.id)
         })
     })
@@ -335,6 +338,120 @@ function editPatch(title, rating, id, genre) {
         .catch(err => console.log(err))
 
 }
+
+
+//Search Movies
+function search_Movie() {
+    let input = document.getElementById('search').value;
+    input = input.toLowerCase();
+    let filteredMovies = [];
+    array.forEach(function(movie) {
+        if (movie.title.toLowerCase().includes(input)) {
+            filteredMovies.push(movie);
+        }
+    });
+    $('.row').innerHTML = renderMovies(filteredMovies);
+    setButtons()
+}
+
+let searchBar = document.querySelector('#search');
+searchBar.addEventListener('keyup', search_Movie);
+
+function renderMovie(movie) {
+
+    let html = "";
+    html += `<div id=${movie.id} class="col-3 mb-3 bg-dark text-light"><p>Title: ${movie.title}</p><p>Rating: ${movie.rating}</p><p>Genre: ${movie.genre}</p>`;
+    html += `<button id=${movie.id}>x</button></div>`;
+    return html
+}
+
+function renderMovies(movies) {
+    $('.row').empty()
+    let html = '';
+    for (let i = 0; i < movies.length; i++) {
+        html += renderMovie(movies[i]);
+    }
+    $('.row').append(html)
+}
+
+
+let range = document.getElementById('ratings')
+range.addEventListener('change', function (e) {
+
+    search_Range(this.value)
+
+})
+
+function search_Range(range) {
+
+    if (range === 'all') {
+        getHandler()
+    }
+    else {
+        let filteredMovies = [];
+        array.forEach(function(movie) {
+            if (movie.rating === range) {
+                filteredMovies.push(movie);
+            }
+        });
+        $('.row').innerHTML = renderMovies(filteredMovies);
+        setButtons()
+    }
+
+
+}
+
+
+function search_Genre() {
+    let input = document.getElementById('searchGenre').value;
+    input = input.toLowerCase();
+    let filteredMovies = [];
+    array.forEach(function(movie) {
+        if (movie.genre.toLowerCase().includes(input)) {
+            filteredMovies.push(movie);
+        }
+    });
+    $('.row').innerHTML = renderMovies(filteredMovies);
+    setButtons()
+}
+
+let genreSearch = document.querySelector('#searchGenre');
+genreSearch.addEventListener('keyup', search_Genre);
+
+
+
+
+// setTimeout(function () {
+//     fetch('http://www.omdbapi.com/?t=alien&apikey=df2cac18&')
+//         .then((response) => response.json())
+//         .then((json) => {
+//             console.log(json)
+//             // let test = document.getElementById('3')
+//
+//             // test.style.backgroundImage = `url('${json.Poster}')`;
+//             // $(test).css({'background-size': 'cover',
+//             //
+//             //     'background-repeat': 'no-repeat',
+//             //
+//             // 'background-position': 'center'
+//             // })
+//
+//
+//         });
+// }, 5000)
+
+let hide = document.getElementById('hide');
+hide.addEventListener('click', function (e) {
+    e.preventDefault();
+    let unhide = document.querySelectorAll('#unhide');
+    unhide.forEach(x => {
+        $(x).toggleClass('none')
+        $('#ratings').toggleClass('none')
+    })
+
+})
+
+
 
 
 // //PUT Method
